34082c7d0ba8652101f4ad6dcc9e44316dc05b3a
Ion Catana <catana.ion17@yahoo.it>
2022-07-19 15:00:55 +0200

    aggiunta di qualche commento

--- Controllers/PizzeController.cs
+++ Controllers/PizzeController.cs
@@ -7,32 +7,36 @@ namespace la_mia_pizzeria_model.Controllers
     {
         [HttpGet]
         public IActionResult Index()
-        {               
+        {   // Aggiungo il nuovo sistema che abbiamo imparato con i DB            
             List<Pizze> pizze = new List<Pizze>();
 
             using (PizzeContext db = new PizzeContext())
             {
                 pizze = db.Pizze.ToList<Pizze>();
             }
-
+            //Il controller dice le liste e il modello
+            //Il controller si chiama la lista delle pizze con il metodo GetPizze()
+            //Poi passo un razor, quindi inserisco il nome della lista, cioé Pize
             return View("HomePage", pizze);
         }
-
+        //Creo una nuova pagina, quindi per prima cosa creo un metodo nuovo
         [HttpGet]
-
+        //Il metodo servira per una determinata pizza descrizione quindi inserisco l'id, il parametro
         public IActionResult Details(int id)
         {
+            //dichiaro un oggetto temporaneo che sará null per ora
             Pizze? pizzaTrovata = null;
 
             using (PizzeContext db = new PizzeContext())
             {
                 pizzaTrovata = db.Pizze.Where(pizze => pizze.Id == id).FirstOrDefault();
             }
-
+            //Se la pizza é stata trovata e quindi e diverso da null, faccio un return vista "Details" 
             if (pizzaTrovata != null)
             {
                 return View("Details", pizzaTrovata);
             }
+            //altrimenti se non é stata trovata inivio un messaggio di errore che segna anche l'id
             else
             {
                 return NotFound("La pizza con l'id " + id + "non è stato trovato");
